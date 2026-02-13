4eca5379580c23945e3f2d22fc35c603587e2caf
Tummyfx <your-alayandetunmishe123@gmail.com>
2026-02-13 10:41:03 +0100

    Added classy classy back to monitor list

--- monitor.js
+++ monitor.js
@@ -11,7 +11,8 @@ console.log("🚀 FAST UPTIME ENGINE INITIATED...");
 const SITES_TO_CHECK = [
     'https://sproutgigs.com',
     'https://en.wikipedia.org',
-    'https://dherhoodsub.ng'
+    'https://dherhoodsub.ng',
+    'https://classyhaven.com.ng'
 ];
 
 // Strict 60-second intervals for professional monitoring
